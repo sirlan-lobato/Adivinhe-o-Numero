c231d196f30e4fd375275f81cebe5fac1956bbe5
sirlan-lobato <sirlanl938@gmail.com>
2023-08-23 20:56:33 -0300

    adicionando a 1 parte do projeto

--- function.js
+++ function.js
@@ -1,1 +1,27 @@
-alert("Hello word")
+let msgBoaVindas = document.getElementById("mensagemBoaVinda");
+let inputNumero = document.getElementById("numeroAleatorio");
+let botao = document.getElementById("botao");
+let numeroAleatorio = parseInt(Math.random() * 10);
+let res = document.getElementById("mensagem");
+let tentativas = 10;
+
+botao.addEventListener('click', ()=>{
+
+    for(let i = 1; i < tentativas; i-- ){
+         if(inputNumero.value == numeroAleatorio){
+        res.innerHTML = `Parabéns vc Acertou o numero era ${numeroAleatorio}`; 
+    }else{
+        if(inputNumero.value > numeroAleatorio){
+            res.innerHTML = `O numero secreto é menor`
+        }else{
+            res.innerHTML = `O numero secreto é maior`
+        }
+    } 
+    }
+
+    alert(`Opa vc ja tentou ${tentativas} tentativas`)
+
+      
+});
+
+console.log(numeroAleatorio)
